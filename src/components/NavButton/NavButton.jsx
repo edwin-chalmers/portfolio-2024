572049d42ceaf37e60d61f4e7d9778c94d91c31d
Edwin Chalmers <edwinchalmers@gmail.com>
2024-06-16 15:36:27 -0600

    Add friendfolio button

--- src/components/NavButton/NavButton.jsx
+++ src/components/NavButton/NavButton.jsx
@@ -1,31 +1,25 @@
 import './NavButton.css';
 
-export default function NavButton({ type, url }) {
-    const iconSrc =
-        type === 'about' 
-        ? './assets/about.svg'
-            : type === 'mail' 
-            ? './assets/mail.svg'
-                : type === 'projects'
-                ? './assets/project.svg'
-                    : type === 'linkedin'
-                    ? './assets/linkedin.svg'
-                        : type === 'github'
-                        ? './assets/github.svg'
-                            : type === 'twitter'
-                            ? './assets/twitter.svg'
-                                : type === 'friends'
-                                ? './assets/friends.svg'
-                                    : './assets/sick.svg'; 
+export default function NavButton({ type, url, onClick }) {
+    const iconSrc = type === 'about' ? './assets/about.svg'
+        : type === 'mail' ? './assets/mail.svg'
+        : type === 'projects' ? './assets/project.svg'
+        : type === 'linkedin' ? './assets/linkedin.svg'
+        : type === 'github' ? './assets/github.svg'
+        : type === 'twitter' ? './assets/twitter.svg'
+        : type === 'friends' ? './assets/friends.svg'
+        : './assets/sick.svg'
 
-    const handleClick = () => {
-        window.open(url, '_blank', 'noopener,noreferrer');
-    };
+    function handleClick() {
+        if(onClick) onClick()
+        window.open(url, '_blank', 'noopener,noreferrer')
+    }
 
     return (
         <button className='nav-button' onClick={handleClick}>
             <img src={iconSrc} alt={`${type} icon`} />
         </button>
-    );
+    )
 }
+
  
